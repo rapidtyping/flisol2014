ec454533eea8628e3a89ed58e426a09507af97ae
Ronny N <rapidtyping@hotmail.com>
2014-03-26 00:23:15 +0000

    Cambiando la configuracion para sqlite3 y agregando una flisol.sqlite3

--- flisol/flisol/settings.py
+++ flisol/flisol/settings.py
@@ -8,14 +8,15 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
+#Agrego esto
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'flisol',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(BASE_DIR,'flisol.sqlite3'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': '123456',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
